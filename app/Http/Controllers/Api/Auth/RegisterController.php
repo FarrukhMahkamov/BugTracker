f4d7715301d1b3500b3301f5677a7838ab3ca7b8
FarrukhMahkamov <farruhmahkamov20@gmail.com>
2022-07-25 00:26:19 +0500

    Add user regiser, login and logout functionality

--- app/Http/Controllers/Api/Auth/RegisterController.php
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -3,9 +3,24 @@
 namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\UserRequest;
+use App\Http\Resources\Auth\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
-    //
+    public function register(UserRequest $request)
+    {
+        $user = User::create([
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => Hash::make($request->password)
+        ]);
+
+        $user->token  = $user->createToken($user->name)->plainTextToken;
+
+        return new UserResource($user);
+    }
 }
